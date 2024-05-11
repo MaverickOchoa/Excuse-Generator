7537449015337f775c2d3baa91bb448508467f65
MaverickOchoa <126739291+MaverickOchoa@users.noreply.github.com>
2024-05-10 19:16:07 -0600

    Add files via upload

---
+++ js.proyect/script.js
@@ -0,0 +1,20 @@
+// Define arrays for different parts of the excuse
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+// Function to generate a random excuse
+function generateExcuse() {
+    let randomWho = who[Math.floor(Math.random() * who.length)];
+    let randomAction = action[Math.floor(Math.random() * action.length)];
+    let randomWhat = what[Math.floor(Math.random() * what.length)];
+    let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+}
+
+// Set the generated excuse to the inner HTML of the #excuse element
+window.onload = function() {
+    document.getElementById('excuse').innerHTML = generateExcuse();
+};
